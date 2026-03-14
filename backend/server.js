c6e584cc10e9065a6f8d37b598da0fbcc10c3feb
ismailpk2098-commits <ismailpk2098@gmail.com>
2026-03-14 22:39:34 +0400

    Update server.js
    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -6,23 +6,26 @@ import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes.js";
 import todoRoutes from "./routes/todoRouters.js";
 
-
 dotenv.config();
 const app = express();
 
-// Middleware
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
+
 app.use(cookieParser());
 app.use(express.json());
 
-// Routes
 app.use("/api/users", userRoutes);
-app.use("/api/todos",todoRoutes);
-
+app.use("/api/todos", todoRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+.then(()=>console.log("MongoDB connected"))
+.catch(err=>console.log(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+app.listen(PORT, ()=>{
+  console.log(`Server running on ${PORT}`);
+});
